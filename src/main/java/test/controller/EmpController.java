049b503089d68e60be7bf2b78b32400b184b5062
ArtekII <216646542+ArtekII@users.noreply.github.com>
2026-06-23 11:52:45 +0300

    Test-S3

---
+++ src/main/java/test/controller/EmpController.java
@@ -0,0 +1,13 @@
+package test.controller;
+
+import pumpkin.annotation.Controller;
+import pumpkin.annotation.UrlMapping;
+
+@Controller
+public class EmpController {
+
+    @UrlMapping(value="/amdrama")
+    public void amdrama() {
+
+    }
+}
