0b1a5cf57ad41c0f1a92c889efd30ae2cd3891c3
ArtekII <216646542+ArtekII@users.noreply.github.com>
2026-07-03 10:03:37 +0300

    fix test

--- src/main/java/test/controller/EmpController.java
+++ src/main/java/test/controller/EmpController.java
@@ -8,22 +8,22 @@ public class EmpController {
 
     @UrlMapping(value="test", method="GET")
     public void test1() {
-        System.out.println("Test Ok");
+        System.out.println("Test GET Ok");
     }
 
     @UrlMapping(value="test", method="POST")
     public void test2() {
-
+        System.out.println("Test POST Ok");
     }
 
     @UrlMapping(value = "error", method = "GET")
     public void error() {
-        throw new RuntimeException("methode executee");
+        System.out.println("Test Error Ok"); //message sera visible dans la console/log Tomcat
     }
 
     @UrlMapping(value = "print", method = "GET")
     public void print() {
-        System.out.println("methode executee"); //message sera visible dans la console/log Tomcat
+        System.out.println("Test Print Ok"); //message sera visible dans la console/log Tomcat
     }
 
     @UrlMapping(value = "hello", method = "GET")
